9d657ed7038c63e8744b14969da870b91aa093c8
Michael Maslennikov <mih.mass@yandex.ru>
2023-03-26 14:45:52 +0500

    2nd assignment complete

--- app/src/main/java/ru/mihmas/aston_3/MainActivity.java
+++ app/src/main/java/ru/mihmas/aston_3/MainActivity.java
@@ -1,8 +1,18 @@
 package ru.mihmas.aston_3;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import ru.mihmas.aston_3.databinding.ActivityMainBinding;
 
@@ -15,5 +25,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         bind = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(bind.getRoot());
+        initView();
+    }
+
+    private void initView() {
+        RequestListener<Drawable> requestListener = new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(
+                    @Nullable GlideException e,
+                    Object model,
+                    Target<Drawable> target,
+                    boolean isFirstResource
+            ) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Картинка не загрузилась :(",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(
+                    Drawable resource,
+                    Object model,
+                    Target<Drawable> target,
+                    DataSource dataSource,
+                    boolean isFirstResource
+            ) {
+                return false;
+            }
+        };
+
+        String url = bind.urlEditText.getText().toString();
+        bind.downloadButton.setOnClickListener(v ->
+                Glide.with(MainActivity.this)
+                        .load(url)
+                        .listener(requestListener)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
+                        .into(bind.image));
     }
 }
